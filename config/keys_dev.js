525e28470e524352c11d3b1d0e8caec494eaef51
Hayden Fuller <hayden.fuller@gmail.com>
2019-12-09 12:28:49 +1100

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI:"mongodb://localhost:27017/mern_app",
-    googleClientID: 
-    "713446049924-kbkq36r0kj83t1nel04hgm9fhkr2gmpr.apps.googleusercontent.com",
-    googleClientSecret:
-        "UvS5ujptb-ZxbamW1ySTlnrTAMqnZ361JR"
-}
