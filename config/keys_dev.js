c471efdbfe655ecdad83fb24e4c350fe90a6b3fd
Ewe Lin Loo <ewelinloo@gmail.com>
2020-01-14 12:38:04 +1100

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:"mongodb://localhost:27017/mern_app",
-    googleClientID: "775355527660-16mn8akdl9kh372i6t376hn8s0819pcc.apps.googleusercontent.com",
-    googleClientSecret: "SSGu0kWCb9x5I4oIFiIi7Efj"
-}
