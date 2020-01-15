f4a4ed0a5920c090a2a457403b3b15275b4fdb4f
Ewe Lin Loo <ewelinloo@gmail.com>
2020-01-15 23:32:22 +1100

    Remove unnecessary imports

--- client/src/components/LogoutMenu.js
+++ client/src/components/LogoutMenu.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
-import { Link } from "react-router-dom";
 
 export default function LogoutMenu (props) {
-
   return <a href="/auth/logout">Logout</a>
   }
