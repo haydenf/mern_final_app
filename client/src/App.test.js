4dd2e924bd83f81b7969c958686ceaa8eb8cb4b5
Ewe Lin Loo <ewelinloo@gmail.com>
2020-01-20 14:34:57 +1100

    Attempt to add test cases

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
 
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const setup = (props={}, state=null) => {
+      return shallow(<App {...props} /> )
+  }
+  
+  const findByTestAttr = (wrapper, val) => {
+      return wrapper.find(`[data-test="${val}"]`);
+  }
+
+  test('renders without crashing', () => {
+        const wrapper = setup();
+        const appComponent = findByTestAttr(wrapper, 'component-app')
+        expect(appComponent.length).toBe(1)
+    });
+
+  test('renders increment button', () => {
+        const wrapper = setup();
+        const button = findByTestAttr(wrapper, 'button')
+        expect(button.length).toBe(1)
+    });
