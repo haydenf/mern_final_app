b440c0059a22e4ee3b291da85dacfb92e6a0a656
Jo Dunham <scaffelpike@yahoo.com.au>
2020-01-21 09:34:49 +1100

    expt with testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -14,13 +14,10 @@ const setup = (props={}, state=null) => {
   }
 
   test('renders without crashing', () => {
-        const wrapper = setup();
-        const appComponent = findByTestAttr(wrapper, 'component-app')
-        expect(appComponent.length).toBe(1)
-    });
-
-  test('renders increment button', () => {
-        const wrapper = setup();
-        const button = findByTestAttr(wrapper, 'button')
-        expect(button.length).toBe(1)
+      const wrapper = shallow(<App />);
+      console.log(wrapper.debug())
+      expect(wrapper).toBeTruthy()
+    //     const wrapper = setup();
+    //     const appComponent = findByTestAttr(wrapper, 'component-app')
+    //     expect(appComponent.length).toBe(1)
     });
