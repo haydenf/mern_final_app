c652c2c04519878c15154f5b63cdffa8478e8fbd
Jo Dunham <scaffelpike@yahoo.com.au>
2020-01-21 15:25:07 +1100

    removed localhost from auth_controller

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -18,7 +18,7 @@ const login = (req, res) => {
     var token = jwt.sign({ sub: req.user._id }, keys.googleClientSecret);
     console.log("..............", req.user._id);
     res.cookie("jwt", token);
-    res.redirect("http://localhost:3000/dashboard?token=" + token);
+    res.redirect("/dashboard?token=" + token);
 }
 
 module.exports = {
