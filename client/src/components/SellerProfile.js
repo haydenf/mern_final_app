b743f15596569ff70c35274bf7979dd68cda244c
Hayden <hayden.fuller@gmail.com>
2020-01-22 16:14:48 +1100

    forgot to import axios, now working

--- client/src/components/SellerProfile.js
+++ client/src/components/SellerProfile.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import { Grid, Header, Container, Image, Divider, List } from 'semantic-ui-react';
 import {connect} from "react-redux";
 import {setUser} from "../actions/userAction"
+import axios from "axios"
 
 
 
